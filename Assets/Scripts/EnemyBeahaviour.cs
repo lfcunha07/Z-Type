a1c0d9c65de50e8fc38dc8d0e101b177b8bf2b01
DiegoSaragozaSilva <torvalds@linux-foundation.org>
2022-11-21 17:53:27 -0300

    UI + Enemy Movement

--- Assets/Scripts/EnemyBeahaviour.cs
+++ Assets/Scripts/EnemyBeahaviour.cs
@@ -7,6 +7,10 @@ public class EnemyBeahaviour : MonoBehaviour
     [SerializeField] private float range;
     [SerializeField] private float speed;
     [SerializeField] private Transform castle;
+    [SerializeField] private Animator anim;
+    [SerializeField] private Rigidbody2D rg2;
+
+    private bool isColliding = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +22,23 @@ public class EnemyBeahaviour : MonoBehaviour
     void Update()
     {
         Vector3 relativePos = castle.position - transform.position;
-        Vector3 desiredUp = new Vector2(relativePos.y, -relativePos.x) * Mathf.Sign(-relativePos.x);
+        Vector3 desiredUp = new Vector3(relativePos.x, relativePos.y, relativePos.z) * Mathf.Sign(relativePos.z);
+        rg2.velocity = relativePos*speed;
         
-        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, desiredUp);   
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, -desiredUp);   
         transform.rotation = rotation;
+        
+        float distance = relativePos.magnitude;
+        if(distance <= range || isColliding){
+            speed = 0;
+            anim.SetBool("inRange", true);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player"){
+            isColliding = true;
+        }
     }
 }
---
+++ Assets/Scripts/LifeBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBar : MonoBehaviour
+{
+    [SerializeField] private PlayerBehaviour life;
+    [SerializeField] private Image lifeBar;
+
+    private float lifeSize;
+
+    void Start()
+    {
+        lifeSize = life.health * 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeSize = life.health * 0.01f;
+        lifeBar.fillAmount = lifeSize;
+    }
+}
